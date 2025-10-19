f7b52353dda9705f3c28cbaa106631413a6508e8
Ravi Shankar <ravidanus@gmail.com>
2025-10-19 17:14:11 +0530

    Files comments added

--- src/main/java/com/example/parent/ParentApplication.java
+++ src/main/java/com/example/parent/ParentApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ParentApplication {
-
+	// Comments added.
 	public static void main(String[] args) {
 		SpringApplication.run(ParentApplication.class, args);
 	}
